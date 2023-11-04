ea453fc338ded20d7a2e030982f0bc29c56c4cbf
afrozshaik-tech <afrozshaik.tech@gmail.com>
2023-11-04 22:18:41 +0530

    1st code

---
+++ test.sh
@@ -0,0 +1,14 @@
+Hi this is afroz
+git init
+git status
+git branch
+git checkout -b test(new-branch-name) (to create branch)
+git checkout -b master(branch-name) (to switch) 
+git add test.sh (to add file into git)
+git commit -m "message" (commit:- this will start version control on file )(-m:- is message)
+*git remote add origin https://github.com/afrozshaik-tech/test.git (this will do only 1st time when a repo is created)
+git branch (to see what is my branch)
+git branch -M master
+
+
+git push -u origin master (now file is pushed)
