b85d814b5d10ab46de5053af2134b5bd6025dee7
afrozshaik-tech <afrozshaik.tech@gmail.com>
2023-11-05 21:03:29 +0530

    git commands

--- test.sh
+++ test.sh
@@ -6,9 +6,8 @@ git checkout -b test(new-branch-name) (to create branch)
 git checkout -b master(branch-name) (to switch) 
 git add test.sh (to add file into git)
 git commit -m "message" (commit:- this will start version control on file )(-m:- is message)
-*git remote add origin https://github.com/afrozshaik-tech/test.git (this will do only 1st time when a repo is created)
+*git remote add origin https://github.com/afrozshaik-tech/test.git (this will do only 1st time when a repo is created)(syntex:- git remote set-url origin https://<token>@github.com/<username>/<repo-name>)
 git branch (to see what is my branch)
 git branch -M master
 git push -u origin master (now file is pushed)
 git log
-git auth
